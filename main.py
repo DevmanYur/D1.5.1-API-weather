d95e9d16b0942693a4edae010052281cea6d1d52
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2023-10-31 11:20:05 +0300

    Добавил города

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ def get_city_characteristics(city,ci,wind,version,language):
     response = requests.get(url)
     response.raise_for_status()
     print(response.text)
+    print()
 
 
 def main():
-    get_city_characteristics("Череповец","m","M","Tnq", "ru")
+    get_city_characteristics("Лондон","m","M","Tnq", "ru")
+    get_city_characteristics("Аэропорт Шереметьево", "m", "M", "Tnq", "ru")
+    get_city_characteristics("Череповец", "m", "M", "Tnq", "ru")
 
 
 if __name__ == '__main__':
