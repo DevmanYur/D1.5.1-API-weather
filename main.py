e30503cf5fbb1823735780c62bd7b88a26b529e6
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-06 16:11:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,30 +6,19 @@ def get_city_characteristics(city,ci,wind,version,language):
     url = url_template.format(city,ci,wind,version,language)
     response = requests.get(url)
     response.raise_for_status()
-    print((response.url))
-    print(response.text)
-    print()
+    return response.text
 
 
 def main():
-    get_city_characteristics("London","m","M","Tnq", "ru")
-    get_city_characteristics("Аэропорт Шереметьево", "m", "M", "Tnq", "ru")
-    get_city_characteristics("Череповец", "m", "M", "Tnq", "ru")
+    print(get_city_characteristics("London","m","M","Tnq", "ru"))
+    print(get_city_characteristics("Аэропорт Шереметьево", "m", "M", "Tnq", "ru"))
+    print(get_city_characteristics("Череповец", "m", "M", "Tnq", "ru"))
 
 
 if __name__ == '__main__':
     main()
 
 
-
-
-payload = {"q": ""}
-
-response = requests.get('https://yandex.ru', params=payload)
-response.raise_for_status()
-
->>> response.url
-'https://yandex.ru/?q='
 
 
 
