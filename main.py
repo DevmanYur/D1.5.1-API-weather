85cb5b01629c78902cf72ced7d5ddf6d41aa3aa6
DevmanYur <devmanyur@gmail.com>
2024-03-07 05:14:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import requests
 
 
-def get_city_characteristics(city,ci,wind,version,language):
-    payload = {ci+wind+version: '', 'lang': language}
-    response = requests.get('http://wttr.in/' + city, params=payload)
+def get_weather(city,ci,wind,version,language):
+    payload = {f'{ci}{wind}{version}': '', 'lang': language}
+    response = requests.get(f'http://wttr.in/{city}', params=payload)
     response.raise_for_status()
     return response.text
 
 
 def main():
-    print(get_city_characteristics("London","m","M","Tnq", "ru"))
-    print(get_city_characteristics("Аэропорт Шереметьево", "m", "M", "Tnq", "ru"))
-    print(get_city_characteristics("Череповец", "m", "M", "Tnq", "ru"))
+    print(get_weather("London","m","M","Tnq", "ru"))
+    print(get_weather("Аэропорт Шереметьево", "m", "M", "Tnq", "ru"))
+    print(get_weather("Череповец", "m", "M", "Tnq", "ru"))
 
 
 if __name__ == '__main__':
