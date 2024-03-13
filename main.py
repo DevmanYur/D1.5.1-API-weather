dfabd1c2434d8e534397d95e684f550ab003cdca
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2024-03-13 09:04:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import requests
 
 
-def get_weather(city,ci,wind,version,language):
-    payload = {f'{ci}{wind}{version}': '', 'lang': language}
+def get_weather(city,parameters):
+    payload = {f'{parameters[0]}{parameters[1]}{parameters[2]}': '', 'lang': parameters[3]}
     response = requests.get(f'http://wttr.in/{city}', params=payload)
     response.raise_for_status()
     return response.text
 
 
 def main():
-    lists_cities = [
-        ("London","m","M","Tnq", "ru"),
-        ("Аэропорт Шереметьево", "m", "M", "Tnq", "ru"),
-        ("Череповец", "m", "M", "Tnq", "ru")
-    ]
+    cities = ["London","Аэропорт Шереметьево", "Череповец"]
+    parameters = ["m","M","Tnq", "ru"]
+    for city in cities:
+        print(get_weather(city,parameters))
 
-    for city in lists_cities:
-        print(get_weather(city[0],city[1],city[2],city[3],city[4]))
 
 if __name__ == '__main__':
     main()
